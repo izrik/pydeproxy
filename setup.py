af7f10be113bbccfd37770b67c000f011da6409a
izrik <izrik@yahoo.com>
2013-03-04 16:23:31 -0600

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(
+    name='deproxy',
+    version='0.1',
+    packages=['deproxy',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
