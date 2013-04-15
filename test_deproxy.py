98498125a9ddecc9449694af4f579925ea5961ad
izrik <izrik@yahoo.com>
2013-04-15 13:37:36 -0500

    Added a test for the echo handler.

--- test_deproxy.py
+++ test_deproxy.py
@@ -40,6 +40,30 @@ class TestDefaultHandler(unittest.TestCase):
         self.assertEquals(int(mc.received_response.code), 200)
 
 
+class TestEchoHandler(unittest.TestCase):
+    def setUp(self):
+        self.deproxy_port = get_next_deproxy_port()
+        self.deproxy = deproxy.Deproxy()
+        self.end_point = self.deproxy.add_endpoint(('localhost',
+                                                    self.deproxy_port))
+
+    def tearDown(self):
+        self.deproxy.shutdown_all_endpoints()
+
+    def test_echo_handler(self):
+        headers = {'x-header': '12345'}
+        mc = self.deproxy.make_request('http://localhost:%i/' %
+                                       self.deproxy_port, headers=headers,
+                                       request_body='this is the body',
+                                       handler_function=deproxy.echo_handler)
+        self.assertEquals(int(mc.received_response.code), 200)
+        self.assertTrue('x-header' in mc.received_response.headers,
+                        msg=('\'x-header\' not in %s' %
+                             mc.received_response.headers))
+        self.assertEquals(mc.received_response.headers['x-header'], '12345')
+        self.assertEquals(mc.received_response.body, 'this is the body')
+
+
 class TestRoute(unittest.TestCase):
     def setUp(self):
         self.deproxy_port = get_next_deproxy_port()
