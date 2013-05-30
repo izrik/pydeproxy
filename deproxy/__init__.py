bf3c7fc818a598421d241425f04aa3824e0f1d40
izrik <izrik@yahoo.com>
2013-05-30 14:54:43 -0500

    Moved Response and the built-in handlers into the __init__.py file.

--- deproxy/__init__.py
+++ deproxy/__init__.py
@@ -30,8 +30,6 @@ version_string = deproxy_version + ' ' + python_version
 
 logger = logging.getLogger(__name__)
 
-from .response import Response
-from .handlers import default_handler, echo_handler, delay, route
 from .handling import Handling
 from .header_collection import HeaderCollection
 from .util import (text_from_file,
@@ -40,6 +38,83 @@ from .util import (text_from_file,
 
 
 request_id_header_name = 'Deproxy-Request-ID'
+
+
+class Response:
+    """A simple HTTP Response, with status code, status message, headers, and
+    body."""
+    def __init__(self, code, message, headers, body):
+        self.code = code
+        self.message = message
+        self.headers = HeaderCollection(headers)
+        self.body = body
+
+    def __repr__(self):
+        return ('Response(code=%r, message=%r, headers=%r, body=%r)' %
+                (self.code, self.message, self.headers, self.body))
+
+
+def default_handler(request):
+    """
+    Handler function.
+    Returns a 200 OK Response, with no additional headers or response body.
+    """
+    logger.debug('')
+    return Response(200, 'OK', {}, '')
+
+
+def echo_handler(request):
+    """
+    Handler function.
+    Returns a 200 OK Response, with the same headers and body as the request.
+    """
+    logger.debug('')
+    return Response(200, 'OK', request.headers, request.body)
+
+
+def delay(timeout, handler_function):
+    """
+    Factory function.
+    Returns a handler that delays the request for the specified number of
+    seconds, forwards it to the next handler function, and returns that
+    handler function's Response.
+    """
+    def delayer(request):
+        logger.debug('delaying for %i seconds' % timeout)
+        time.sleep(timeout)
+        return handler_function(request)
+
+    delayer.__doc__ = ('Delay for %s seconds, then forward the Request to the '
+                       'next handler' % str(timeout))
+
+    return delayer
+
+
+def route(scheme, host, deproxy):
+    """
+    Factory function.
+    Returns a handler that forwards the request to a specified URL, using
+    either HTTP or HTTPS (regardless of what protocol was used in the initial
+    request), and returning the response from the host so routed to.
+    """
+    logger.debug('')
+
+    def route_to_host(request):
+        logger.debug('request = %s,%s,%s' % (request.method, request.path,
+                                             request.protocol))
+        logger.debug('scheme, host = %s, %s' % (scheme, host))
+        request2 = Request(request.method, request.path, 'HTTP/1.0',
+                           request.headers, request.body)
+        if 'Host' in request2.headers:
+            request2.headers.delete_all('Host')
+        logger.debug('sending request')
+        response = deproxy.send_request(scheme, host, request2)
+        logger.debug('received response')
+        return response, False
+
+    route_to_host.__doc__ = "Route responses to %s using %s" % (host, scheme)
+
+    return route_to_host
 
 
 class Deproxy:
@@ -748,3 +823,5 @@ class Request:
     def __repr__(self):
         return ('Request(method=%r, path=%r, headers=%r, body=%r)' %
                 (self.method, self.path, self.headers, self.body))
+
+
