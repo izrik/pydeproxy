a107127d183d59545ce3c864729c1179a6bee463
izrik <izrik@yahoo.com>
2013-05-29 21:10:52 -0500

    Added the read_body_from_stream function.

--- deproxy/util.py
+++ deproxy/util.py
@@ -22,3 +22,21 @@ def lines_from_file(file):
         return s.splitlines()
     except AttributeError:
         return str(file).splitlines()
+
+
+def read_body_from_stream(stream, headers):
+    if ('Transfer-Encoding' in headers and
+            headers['Transfer-Encoding'] != 'identity'):
+        # 2
+        raise NotImplementedError
+    elif 'Content-Length' in headers:
+        # 3
+        length = int(headers['Content-Length'])
+        body = stream.read(length)
+    elif False:
+        # multipart/byteranges ?
+        raise NotImplementedError
+    else:
+        # there is no body
+        body = None
+    return body
