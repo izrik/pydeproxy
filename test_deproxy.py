f977023ae466cde7c6876d1143482d2c30c2b294
izrik <izrik@yahoo.com>
2013-05-30 13:30:36 -0500

    Added tests for bodies.

--- test_deproxy.py
+++ test_deproxy.py
@@ -354,6 +354,51 @@ class TestHeaderCollection(unittest.TestCase):
         self.assertEqual(self.headers.get('name', default='zxcv'), 'Value')
         self.assertEqual(self.headers.get('asdf', default='zxcv'), 'zxcv')
 
+
+class TestBodies(unittest.TestCase):
+    def setUp(self):
+        self.deproxy = deproxy.Deproxy()
+        self.port = get_next_deproxy_port()
+        self.deproxy.add_endpoint(('localhost', self.port))
+        self.url = 'http://localhost:{0}/'.format(self.port)
+
+    def test_request_body(self):
+        body = """ This is the body
+
+        This is the next paragraph.
+        """
+        mc = self.deproxy.make_request(url=self.url, method='POST',
+                                       request_body=body)
+        self.assertEqual(mc.sent_request.body, body)
+        self.assertEqual(len(mc.handlings), 1)
+        self.assertEqual(mc.handlings[0].request.body, body)
+
+    def test_response_body(self):
+        body = """ This is another body
+
+        This is the next paragraph.
+        """
+        def custom_handler(request):
+            return deproxy.Response(code=200, message='OK', headers=None,
+                                    body=body)
+        mc = self.deproxy.make_request(url=self.url,
+                                       handler_function=custom_handler)
+        self.assertEqual(mc.received_response.body, body)
+        self.assertEqual(len(mc.handlings), 1)
+        self.assertEqual(mc.handlings[0].response.body, body)
+
+    @unittest.expectedFailure
+    def test_request_body_chunked(self):
+        self.fail()
+
+    @unittest.expectedFailure
+    def test_response_body_chunked(self):
+        self.fail()
+
+    def tearDown(self):
+        self.deproxy.shutdown_all_endpoints()
+
+
 def run():
     parser = argparse.ArgumentParser()
     parser.add_argument('--port-base', help='The base port number to use when '
