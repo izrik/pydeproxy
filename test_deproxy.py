7c9f7c8a7b17989a8fb7ccedb145a37f48443eec
izrik <izrik@yahoo.com>
2013-05-31 10:15:44 -0500

    Expanded tests for endpoint default handler.

--- test_deproxy.py
+++ test_deproxy.py
@@ -134,7 +134,7 @@ class TestEndpointDefaultHandler(unittest.TestCase):
         self.port = get_next_deproxy_port()
         self.deproxy = deproxy.Deproxy()
 
-    def test_endpoint_default_handler(self):
+    def test_endpoint_default_handler_function(self):
         def custom_handler(request):
             return deproxy.Response(code='601', message='Custom', headers={},
                                     body=None)
@@ -146,6 +146,20 @@ class TestEndpointDefaultHandler(unittest.TestCase):
         self.assertEqual(len(mc.handlings), 1)
         self.assertEqual(mc.handlings[0].response.code, '601')
         self.assertEqual(mc.received_response.code, '601')
+
+    def custom_handler_method(self, request):
+        return deproxy.Response(code='602', message='Custom', headers={},
+                                body=None)
+
+    def test_endpoint_default_handler_method(self):
+        self.deproxy.add_endpoint(port=self.port,
+                                  default_handler=self.custom_handler_method)
+        url = 'http://localhost:{0}/'.format(self.port)
+        mc = self.deproxy.make_request(url=url)
+
+        self.assertEqual(len(mc.handlings), 1)
+        self.assertEqual(mc.handlings[0].response.code, '602')
+        self.assertEqual(mc.received_response.code, '602')
 
     def tearDown(self):
         self.deproxy.shutdown_all_endpoints()
